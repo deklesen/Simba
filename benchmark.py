c2d64489f0d47b54527c0dafc8a9e08626d7cff1
Jonas Klesen <s8jokles@stud.uni-saarland.de>
2020-10-26 06:58:03 +0100

    More infection rates

--- benchmark.py
+++ benchmark.py
@@ -165,7 +165,7 @@ graphs=filter_graphs({
     **{f'barabasi_{node_num}': barabasi(node_num) for node_num in [25,50,100,250,500,1000]},
 })
 
-infection_rates = [1.5]
+infection_rates = [1,1.5,2]
 
 init_infecteds_fraction=[0.025,0.05,0.075,0.1,0.15,0.25]
 budgets_fraction=[0.05,0.075,0.1,0.15,0.25]
